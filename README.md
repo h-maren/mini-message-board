3bf0af5b00c2b9ef078fe48fa6c3bb56ccda0977
h-maren <26232423+h-maren@users.noreply.github.com>
2025-01-19 12:29:47 -0500

    Add postgresql and generate db in populatedb.js Need to still move everything else from messages array

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mini-message-board
 mini message board using express and ejs, mvc model
+
+revision beginning 1/19 to use postgresql to generate database
---
+++ db/populatedb.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+
+const { Client } = require("pg");
+require("dotenv").config();
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS messages (
+id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+ message VARCHAR ( 255 ),
+ username VARCHAR ( 255 ),
+ dateadded TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO messages (message,username,dateadded) 
+VALUES
+  ('Mama always said life was like a box of chocolates','Forrest Gump',CURRENT_TIMESTAMP),
+  ('You talking to me\?','Travis Bickle',CURRENT_TIMESTAMP),
+  ('I''m scared, all right\?\!','Rocky Balboa',CURRENT_TIMESTAMP)
+;`
+
+async function main() {
+    console.log("seeding...");
+    const client = new Client({
+      host: "localhost", // or wherever the db is hosted
+      user: process.env.ROLE,
+      database: "messageboard",
+      password: process.env.PASSWORD,
+      port: 5432 // The default port
+    });
+    await client.connect();
+    await client.query(SQL);
+    await client.end();
+    console.log("done");
+  }
+
+main();
